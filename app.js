00b2af7f42488652e48951ae839bea27aec0c52b
KevinVishalSaldanha <vishalkevin11@gmail.com>
2021-04-03 08:58:12 +0530

    Added base imports

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+require('dotenv/config');
+
+const app = express();
+
+//middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+app.use(helmet());
+
+//Redirect request to respective classes
+app.use('/', (req, res, next) => {
+    res.status(401).json({
+        'message': 'Access Denied'
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server Started');
+});
